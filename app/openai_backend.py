a36f941da3a4ade41b53bc85cf5cebaafd622f5b
gettodaze <jmccloskey2@wisc.edu>
2022-09-04 19:19:05 -0700

    rewrite parser

--- app/openai_backend.py
+++ app/openai_backend.py
@@ -1,11 +1,33 @@
-import os
+from __future__ import annotations
+
+import argparse
 import openai
 
 from app.config import OPENAI_API_KEY
 
 openai.api_key = OPENAI_API_KEY
-response = openai.Completion.create(
-    model="text-davinci-002", prompt="Say this is a test", max_tokens=6, temperature=0
-)
 
-print(response)
+
+def get_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("idea", type=str)
+    return parser
+
+
+def get_openai_text(idea: str):
+    return openai.Completion.create(
+        model="davinci-instruct-beta-v3",
+        prompt=f"Generate a snippet about a cat {idea}:",
+        max_tokens=32,
+        temperature=0,
+    )["choices"][0]["text"]
+
+
+def main(argv: list[str] | None = None) -> None:
+    parser = get_argparser()
+    config = parser.parse_args(argv)
+    print(get_openai_text(config.idea))
+
+
+if __name__ == "__main__":
+    main()
